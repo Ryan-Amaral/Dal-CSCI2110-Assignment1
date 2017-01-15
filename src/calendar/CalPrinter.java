c7290b3f7d135114587406c350b2960369e79d4b
Ryan-Amaral <ryan_amaral@live.com>
2017-01-15 17:21:52 -0400

    calendar works with user input

--- src/calendar/CalPrinter.java
+++ src/calendar/CalPrinter.java
@@ -70,9 +70,87 @@ public class CalPrinter
 	
 	public static void main(String[] args)
 	{	
+		String input; // temp space used for inputs from user
+		Boolean isValid = false; // boolean used to determine if input is valid
 		
+		// rows and columns of months
+		int rows = 3;
+		int cols = 4;
+		
+		int year = 2017; // the year to use
+		
+		Boolean isFullNames = true; // whether to use full or short month names
+		
+		System.out.println("Hello, welcome to The Calendar Printer.\n"
+				+ "Please enter the amount of rows and columns of months "
+				+ "that you want.");
+
+		Scanner scanner = new Scanner(System.in); // to get input
+		
+		// the pattern that the dimension input must be in
+		final String dimensionPattern = "\\d{1,2}(x|,| |X)\\d{1,2}";
+		// the pattern for a year (accepts from 0 to 9999)
+		final String yearPattern = "\\d{1,4}";
+		
+		// repeatedly ask for input until it is valid
+		while(!isValid){
+			System.out.println("Please enter in the form '#,#', or '#x#', "
+					+ "or '# #'. Rows and columns must multiply to equal 12:");
+			input = scanner.nextLine();
+			if(input.matches(dimensionPattern)){
+				String tmpStrs[] = input.split("(,|x|X| )");
+				try{
+					rows = Integer.parseInt(tmpStrs[0]);
+					cols = Integer.parseInt(tmpStrs[1]);
+					if(rows * cols == 12)
+						isValid = true;
+					else
+						System.out.println("Invalid amount of rows and columns. "
+								+ "Please try again.");
+				}catch(Exception e){
+					System.out.println("An error has occured with your input, "
+							+ "please try again.");
+				}
+			}
+			else
+				System.out.println("Your input did not match the pattern, "
+						+ "please try again.");
+		}
+		
+		isValid = false; // reset validity flag
+		
+		// dimensions of calendar has been entered, now to get year
+		while(!isValid){
+			System.out.println("Please enter a year:");
+			input = scanner.nextLine();
+			if(input.matches(yearPattern)){
+				try{
+					year = Integer.parseInt(input);
+					isValid = true;
+				}catch(Exception e){
+					System.out.println("An error has occured with your input, "
+							+ "please try again.");
+				}
+			}
+			else
+				System.out.println("Your input was not valid, please try again.");
+		}
+		
+		// now to see if want full or short month names
+		System.out.println("Do you want to use full month names? "
+				+ "Enter 'y' for yes, otherwise the calendar will "
+				+ "use shortened versions of the month names.");
+		
+		input = scanner.nextLine();
+		
+		if(input.equals("y") || input.equals("Y"))
+			isFullNames = true;
+		else
+			isFullNames = false;
+		
+		System.out.println(); // extra blank line
 		
 		//Test printing a Calendar
-		printCalendar(2017, 3, 4, true);
+		printCalendar(year, rows, cols, isFullNames);
 	}
 }
